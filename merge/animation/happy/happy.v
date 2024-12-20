880443af91f80ba4dbda949d1682512bceeacdf8
JustinShih0918 <72808967+JustinShih0918@users.noreply.github.com>
2024-12-21 02:35:43 +0800

    asy rst

--- merge/animation/happy/happy.v
+++ merge/animation/happy/happy.v
@@ -9,7 +9,7 @@ module happy (
     wire clk_25;
     clock_divider #(.n(25)) clock_divider_inst (.clk(clk), .clk_div(clk_25));
     happy_mem happy_mem_inst (.step(step), .ram_addr_x(ram_addr_x), .ram_addr_y(ram_addr_y), .ram_data(ram_data));
-    always @(posedge clk_25) begin
+    always @(posedge clk_25, posedge rst) begin
         if(rst || step > 15) step <= 0;
         else step <= step + 1;   
     end
